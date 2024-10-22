09c11034f1dd218407fcaeb4964bd32e313ced03
Epangelias <epangelias@gmail.com>
2024-10-22 15:38:47 -0500

    set formatting options

--- tasks/open.ts
+++ tasks/open.ts
@@ -1,27 +1,27 @@
 #!/usr/bin/env -S deno run -A
 
-Deno.chdir(import.meta.dirname?.split("/").slice(0, -1).join("/") as string);
+Deno.chdir(import.meta.dirname?.split('/').slice(0, -1).join('/') as string)
 
 const exec = async (args: string[]) =>
     await new Deno.Command(args[0], {
         args: args.slice(1),
-        stdin: "inherit",
-        stdout: "inherit",
-        stderr: "inherit",
+        stdin: 'inherit',
+        stdout: 'inherit',
+        stderr: 'inherit',
     })
-        .output();
+        .output()
 
-await exec(["code", "."]);
+await exec(['code', '.'])
 
 // Run synchronously
-exec([Deno.execPath(), "-A", "tasks/dev.ts"]);
+exec([Deno.execPath(), '-A', 'tasks/dev.ts'])
 
 // Wait to launch page in browser until loaded
-await new Promise((resolve) => setTimeout(resolve, 500));
+await new Promise((resolve) => setTimeout(resolve, 500))
 
 // Open Browser page
-if (Deno.build.os == "linux") {
-    await exec(["xdg-open", "http://0.0.0.0:8000/"]);
-} else if (Deno.build.os == "darwin") {
-    await exec(["open", "http://0.0.0.0:8000/"]);
+if (Deno.build.os == 'linux') {
+    await exec(['xdg-open', 'http://0.0.0.0:8000/'])
+} else if (Deno.build.os == 'darwin') {
+    await exec(['open', 'http://0.0.0.0:8000/'])
 }
