73b86b8ee20501d8557ace1c1333e65f4f3bb626
Epangelias <epangelias@gmail.com>
2024-10-22 13:12:18 -0500

    working on init project

--- tasks/initproject.ts
+++ tasks/initproject.ts
@@ -1,1 +1,18 @@
 #!/usr/bin/env -S deno run -A
+
+const projectName = Deno.args[0] || prompt("Enter project name") ||
+    "fresh-project";
+
+const exec = async (args: string[]) =>
+    await new Deno.Command(args[0], { args: args.slice(1) }).output();
+
+await exec([
+    "git",
+    "clone",
+    "https://github.com/epangelias/fresh-tempalte.git",
+    projectName,
+]);
+
+Deno.chdir(projectName);
+
+await exec(["deno", "task", "init"]);
