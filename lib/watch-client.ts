27a548fd763c6093d3cd446a2208349c259339c2
Epangelias <epangelias@gmail.com>
2024-10-29 19:41:43 -0500

    added reconnection for websockets

--- lib/watch-client.ts
+++ lib/watch-client.ts
@@ -1,13 +1,34 @@
-import { IS_BROWSER } from 'fresh/runtime';
 
 
 export function watchClient<T>(endpoint: string, callback: (data: T) => void) {
-    const websocket = new WebSocket(endpoint);
+    let websocket: WebSocket;
+    let reconnectAttempts = 0;
 
-    websocket.onmessage = (e) => {
-        const data = JSON.parse(e.data);
-        callback(data);
+    function connect() {
+        websocket = new WebSocket(endpoint);
+
+        websocket.onmessage = (e) => {
+            const data = JSON.parse(e.data);
+            callback(data);
+        };
+
+        websocket.onclose = () => reconnect();
+
+        websocket.onerror = (_error) => websocket.close();
+    }
+
+    async function reconnect() {
+        const delay = Math.min(1000 * Math.pow(2, reconnectAttempts), 30000);
+        await new Promise((resolve) => setTimeout(resolve, delay));
+        reconnectAttempts += 1;
+        console.log(`Reconnecting... attempt ${reconnectAttempts}`);
+        connect();
+    }
+
+    connect();
+
+    return () => {
+        websocket.close();
+        reconnectAttempts = 0;
     };
-
-    return () => websocket.close()
-};
+}
