6fba0d92aa6ddae58a98f6fbb8217b0c92e54865
Epangelias <epangelias@gmail.com>
2024-10-30 07:52:20 -0500

    refactor

--- islands/ChatBox.tsx
+++ islands/ChatBox.tsx
@@ -3,12 +3,12 @@ import { watchData } from '../lib/watch-data.ts';
 import { CounterData } from '@/lib/types.ts';
 
 export default function Counter({ data }: { data: CounterData }) {
-  const counterData = useSignal(data);
+  const counterData = useSignal<CounterData>(data);
 
-  watchData('/api/counter', counterData);
+  watchData('/api/watch', counterData);
 
   return (
-    <div class='counter text-center'>
+    <div class='chat-box'>
       <button onClick={() => counterData.value = { count: counterData.value.count - 1 }}>- 1</button>{' '}
       <button onClick={() => counterData.value = { count: counterData.value.count + 1 }}>+ 1</button>
       <h2>{counterData.value.count}</h2>
---
+++ lib/handle-watch.ts
@@ -0,0 +1,31 @@
+import { FreshContext } from 'fresh';
+import { db } from '@/lib/db.ts';
+
+
+export function handleWatchData(ctx: FreshContext, key: Deno.KvKey) {
+    if (ctx.req.headers.get('upgrade') != 'websocket') return new Response(null, { status: 501 });
+
+    const { socket, response } = Deno.upgradeWebSocket(ctx.req);
+    let currentData = '';
+
+    socket.onopen = async () => {
+        for await (const _event of db.watch([key])) {
+            const { value } = await db.get(key);
+            const json = JSON.stringify(value);
+            if (json == currentData) continue;
+            currentData = json;
+            if (socket.readyState == 1) socket.send(json);
+        }
+    };
+
+    socket.onerror = (event) => {
+        console.log(`Error watching ${key}: ${event}`);
+    };
+
+    socket.onmessage = (event) => {
+        currentData = event.data;
+        db.set(key, JSON.parse(event.data));
+    };
+
+    return response;
+}
