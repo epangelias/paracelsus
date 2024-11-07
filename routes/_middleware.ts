519a80a60e396df67cea3745430bf3912b41b6df
Epangelias <epangelias@gmail.com>
2024-11-07 12:48:30 -0500

    remove middleware

--- routes/_middleware.ts
+++
@@ -1,9 +0,0 @@
-import { define, State } from '@/lib/utils.ts';
-import { FreshContext } from 'fresh';
-
-
-export default define.middleware((ctx) => {
-    console.log(`${ctx.req.method} ${ctx.req.url}`);
-    return ctx.next();
-});
-
