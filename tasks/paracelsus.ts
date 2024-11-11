9d6ef5a975956122d8c415a4797a47ea27f432c7
Epangelias <epangelias@gmail.com>
2024-11-11 09:28:48 -0800

    task changes

--- tasks/paracelsus.ts
+++ tasks/paracelsus.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno run -A
 
-import { exec } from '@/lib/utils.ts';
+import { exec, getParentDir } from '@/lib/utils.ts';
 
 const projectName = Deno.args[0] ||
     prompt('Enter Project Name [fresh-project]') ||
@@ -15,5 +15,35 @@ await exec([
 
 Deno.chdir(projectName);
 
-await exec(['deno', 'task', 'init', projectName]);
+//
+// Initial Project
+//
+
+const path = (p: string) => `${getParentDir(import.meta.dirname)}/${p}`;
+
+// Remove Github Repo
+await Deno.remove(path('.git'), { recursive: true });
+
+// Set Site Data
+const siteData = `export const site = {
+    name: ${projectName},
+    favicon: Meth.emojiToUrl("${prompt("Emoji Icon [🔥]") || "🔥"}"),
+    themeColor: "#eb9a52",
+    description: ${prompt("Project Description") || ""},
+    email: "vaza@vaza.app",
+    lang: "en-US"
+};`
+Deno.writeTextFile(path('lib/site.ts'), siteData);
+
+// Remove Readme
+await Deno.remove(path('README.md'));
+
+// Remove Tasks
+await Deno.remove(path('tasks/paracelsus.ts'));
+
+// Open VSCODE
+await exec([Deno.execPath(), 'task', 'open']);
+
 await exec(['deno', 'task', 'update']);
+
+await exec(['exit']);
