123f3d6ea918827799ff52a16c62050673878e30
Epangelias <epangelias@gmail.com>
2024-11-12 21:20:09 -0800

    change icon

--- lib/site.ts
+++ lib/site.ts
@@ -2,7 +2,8 @@ import { Meth } from '@/lib/meth.ts';
 
 export const site = {
     name: "Paracelsus",
-    favicon: Meth.emojiToUrl("🌀"),
+    // favicon: Meth.emojiToUrl("🧙‍♂️"),
+    favicon: "/img/favicon.png",
     themeColor: "#eb9a52",
     description: "Talk to Paracelsus",
     email: "vaza@vaza.app",
