366c79acb8f67a3919666dddca8723fa0435d87c
Epangelias <epangelias@gmail.com>
2024-11-16 07:44:14 -0800

    add icons

--- lib/site.ts
+++ lib/site.ts
@@ -3,6 +3,7 @@ export const site = {
   // favicon: Meth.emojiToUrl("🧙‍♂️"),
   favicon: '/img/favicon.png',
   appIcon: '/img/app.png',
+  previewImage: '', // 1200x630
   themeColor: '#eb9a52',
   description: 'Talk to Paracelsus',
   email: 'vaza@vaza.app',
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -9,17 +9,24 @@ export default define.page(({ Component, state }) => {
     <html lang={site.lang}>
       <head>
         <title>{site.name}</title>
+        <meta content={site.name} property='og:title'></meta>
+        <meta content={site.description} name='description' />
+        <meta content={site.description} property='og:description' />
+        <meta content={site.previewImage} property='og:image' />
+        <meta property='og:type' content='website' />
+
         <meta charset='utf-8' />
-        <meta name='description' content={site.description} />
         <meta
-          content='minimum-scale=1.0, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'
           name='viewport'
+          content='width=device-width,height=device-height,initial-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover'
         />
         <meta name='color-scheme' content='light dark' />
         <link rel='apple-touch-icon' href={site.appIcon} />
+        <meta name='mobile-web-app-capable' content='yes' />
         <meta name='apple-mobile-web-app-capable' content='yes' />
         <meta name='msapplication-tap-highlight' content='no' />
         <meta name='theme-color' content={site.themeColor} />
+        <meta name='format-detection' content='telephone=no' />
         <link rel='manifest' href='/manifest.json' />
         <link rel='stylesheet' href='/css.css' />
         <link rel='icon' href={site.favicon} />
