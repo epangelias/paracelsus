53922f239537553a674401bdd03896162c33e07f
Epangelias <epangelias@gmail.com>
2024-11-16 07:49:04 -0800

    manifest changes

--- routes/manifest.json.ts
+++ routes/manifest.json.ts
@@ -1,9 +1,12 @@
 import { define } from '@/lib/utils.ts';
 import { site } from "@/lib/site.ts";
+import icons from "@/static/AppImages/icons.json"  with { type: "json" };
 
 export const handler = define.handlers(() => {
   return Response.json({
     name: site.name,
+    short_name: site.name,
+    id: site.name.toLowerCase(),
     start_url: '/',
     lang: site.lang,
     theme_color: site.themeColor,
@@ -14,6 +17,7 @@ export const handler = define.handlers(() => {
         src: site.favicon,
         sizes: 'any',
       },
+      ...icons.icons.map(icon => ({ ...icon, src: "/AppImages/" + icon.src }))
     ],
   });
 });
