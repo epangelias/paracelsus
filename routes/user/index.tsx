aede59ba97f65c27ce5721f88f5cfc3d41b86c5f
Epangelias <epangelias@gmail.com>
2024-11-23 15:47:52 -0800

    attempt

--- routes/user/index.tsx
+++ routes/user/index.tsx
@@ -4,23 +4,22 @@ import { updateUser } from '@/lib/user-data.ts';
 // import { UserUI } from '@/islands/UserUI.tsx';
 import { STATUS_CODE } from '@std/http/status';
 import { Meth } from '@/lib/meth.ts';
-// import { sendEmailVerification } from '@/lib/mail.ts';
+import { sendEmailVerification } from '@/lib/mail.ts';
 import { Page } from '@/components/Page.tsx';
 
 export const handler = define.handlers({
   GET: async (ctx) => {
-    return new Response();
-    // const user = ctx.state.user;
-    // if (!user) throw new HttpError(STATUS_CODE.Unauthorized);
-    // if (ctx.req.method == 'GET') return page();
-    // try {
-    //   const { email, name } = Meth.formDataToObject(await ctx.req.formData());
-    //   const newUser = await updateUser({ ...user, name, email });
-    //   if (newUser.email != user.email) await sendEmailVerification(ctx.url.origin, user);
-    //   return page({ message: 'Saved!', error: undefined });
-    // } catch (e) {
-    //   return page({ message: undefined, error: Meth.getErrorMessage(e) });
-    // }
+    const user = ctx.state.user;
+    if (!user) throw new HttpError(STATUS_CODE.Unauthorized);
+    if (ctx.req.method == 'GET') return page();
+    try {
+      const { email, name } = Meth.formDataToObject(await ctx.req.formData());
+      const newUser = await updateUser({ ...user, name, email });
+      if (newUser.email != user.email) await sendEmailVerification(ctx.url.origin, user);
+      return page({ message: 'Saved!', error: undefined });
+    } catch (e) {
+      return page({ message: undefined, error: Meth.getErrorMessage(e) });
+    }
   },
 });
 
