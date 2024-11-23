a0ac25b9c5e06b2caff859139459a75b77850058
Epangelias <epangelias@gmail.com>
2024-11-23 15:51:46 -0800

    attempt

--- lib/mail.ts
+++ lib/mail.ts
@@ -5,21 +5,21 @@ import Mailjet from 'node-mailjet';
 import { asset } from 'fresh/runtime';
 import { RateLimiter } from '@/lib/rate-limiter.ts';
 
-const limiter = new RateLimiter({ maxRequests: 2, interval: 60 }); // 2 per minute
+// const limiter = new RateLimiter({ maxRequests: 2, interval: 60 }); // 2 per minute
 
 let mailjet: Mailjet.Client;
 
 if (!Deno.env.has('MJ_APIKEY_PUBLIC') || !Deno.env.has('MJ_APIKEY_PRIVATE')) {
   console.warn('Missing mailjet credentials');
 } else {
-  mailjet = new Mailjet.Client({
-    apiKey: Deno.env.get('MJ_APIKEY_PUBLIC'),
-    apiSecret: Deno.env.get('MJ_APIKEY_PRIVATE'),
-  });
+  // mailjet = new Mailjet.Client({
+  //   apiKey: Deno.env.get('MJ_APIKEY_PUBLIC'),
+  //   apiSecret: Deno.env.get('MJ_APIKEY_PRIVATE'),
+  // });
 }
 
 export async function sendMail(options: MailOptions) {
-  limiter.request();
+  // limiter.request();
 
   if (!mailjet) throw new Error('Mail Disabled');
 
