9f4dbaddb2951dcc4b084dfa34dd6ff2c3495c05
Epangelias <epangelias@gmail.com>
2024-11-23 16:42:29 -0800

    fix pwa detection

--- islands/Banner.tsx
+++ islands/Banner.tsx
@@ -33,7 +33,7 @@ export function Banners() {
     },
     {
       name: 'ios-install',
-      condition: () => global.pwa.isPWA.value && isIOSSafari(),
+      condition: () => !global.pwa.isPWA.value && isIOSSafari(),
       canClose: true,
       content: () => <a href='/install-guide-ios'>Install this app to your device</a>,
     },
