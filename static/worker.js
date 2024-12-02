57ec4ac9dff2cdd45239342ecd5ad7c9f1aff790
Epangelias <epangelias@gmail.com>
2024-12-02 12:31:31 -0800

    fix offline feature

--- static/worker.js
+++ static/worker.js
@@ -7,6 +7,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   const url = new URL(event.request.url);
   const isAPI = url.pathname.startsWith('/api');
+  if (isAPI) return;
   event.respondWith(
     fetch(event.request).catch((e) => isAPI ? e : caches.match('/offline')),
   );
