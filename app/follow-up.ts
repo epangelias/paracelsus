ed8fb64c2ac381bcb05bb5a449823bc978521afa
Epangelias <epangelias@gmail.com>
2024-12-09 13:43:48 -0800

    log

--- app/follow-up.ts
+++ app/follow-up.ts
@@ -14,6 +14,7 @@ async function generateFollowUpMessage(user: UserData) {
     content:
       `SYSTEM COMMAND: Follow up to ${user.name} in one sentence to start a conversation with them, you may ask them a question too.`,
   });
+  console.log({ messages });
   const res = await generateChatCompletion(undefined, messages);
   const content = res.choices[0].message.content;
   if (!content) throw new Error('No content generated: ' + JSON.stringify(res));
