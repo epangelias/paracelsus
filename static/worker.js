de55c5ba9587ed55b2a0a19a0088e6a014ad18d6
Epangelias <epangelias@gmail.com>
2025-03-05 11:52:57 -0800

    log

--- static/worker.js
+++ static/worker.js
@@ -5,6 +5,7 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
   event.waitUntil(
     caches.open('offline-cache').then((cache) => cache.delete('/').then(() => cache.add('/'))),
   );
@@ -25,15 +26,11 @@ self.addEventListener('fetch', (event) => {
 });
 
 self.addEventListener('push', (event) => {
-  const text = event.data.text();
-  const data = !text.startsWith('{') ? { title: text } : event.data.json();
-  console.log('Received push', data);
+  const data = !text.startsWith('{') ? { title: event.data.text() } : event.data.json();
   event.waitUntil(self.registration.showNotification(data.title, data));
 });
 
 self.addEventListener('notificationclick', function (event) {
   event.notification.close();
-  event.waitUntil(
-    clients.openWindow(self.origin),
-  );
+  event.waitUntil(clients.openWindow(self.origin));
 });
