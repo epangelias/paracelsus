865a7df04c3c5f240f090f409df05f816b758980
Epangelias <epangelias@gmail.com>
2025-03-05 17:44:12 -0800

    change offline page

--- pages/offline.md
+++
@@ -1,7 +0,0 @@
----
-title: Your Offline
----
-
-# Your Offline
-
-[Back](/)
---
+++ routes/(app)/offline.tsx
@@ -0,0 +1,11 @@
+import { Page } from '@/components/Page.tsx';
+import { define } from '@/lib/utils/utils.ts';
+
+export default define.page(() => (
+  <Page hideBanner={true} hideHeader={true}>
+    <h1>Your Offline!</h1>
+    <p>
+      <a href='/'>Reload</a>
+    </p>
+  </Page>
+));
